51a8315d4f77994fbf14b6b9b1da0dc1c68ffaaa
iuliiaglechik <yuliashmeleva007@gmail.com>
2026-09-13 18:49:15 +0500

    Create Color.kt

---
+++ app/src/main/java/com/nonogram/mvp/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.nonogram.mvp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PaperBackground = Color(0xFFF7F4EE)
+val PaperSurface = Color(0xFFFFFFFF)
+val InkPrimary = Color(0xFF1C1B19)
+val InkSecondary = Color(0xFF6B6560)
+val GridLine = Color(0xFFDAD5CB)
+val GridLineStrong = Color(0xFFB9B2A4)
+val CellFilled = Color(0xFF1C1B19)
+val CellMarked = Color(0xFFB9B2A4)
+val Accent = Color(0xFFC97B3D)
+val AccentSoft = Color(0xFFEFE0CF)
+val ErrorSoft = Color(0xFFB3543F)
+
+val CharcoalBackground = Color(0xFF15140F)
+val CharcoalSurface = Color(0xFF1F1D18)
+val InkPrimaryDark = Color(0xFFF2EFE9)
+val InkSecondaryDark = Color(0xFFA39C90)
+val GridLineDark = Color(0xFF3A362E)
+val GridLineStrongDark = Color(0xFF554F42)
+val CellFilledDark = Color(0xFFF2EFE9)
+val CellMarkedDark = Color(0xFF6E6858)
+val AccentDark = Color(0xFFE0A15E)
+val AccentSoftDark = Color(0xFF3A2E1E)
